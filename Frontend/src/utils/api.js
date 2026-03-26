b3c2e7405fb48cc68c4eb4c703b38f3ba53de340
Mohitkr3041 <mohitkumar9pro@gmail.com>
2026-03-26 20:17:07 +0530

    Updated backend auth and API integration new

--- Frontend/src/utils/api.js
+++ Frontend/src/utils/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: "https://aistro-01sa.onrender.com",
-  withCredentials: true,
+  withCredentials: true
 });
 
 export default api;
