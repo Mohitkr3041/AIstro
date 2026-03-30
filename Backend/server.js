c7f8184860ccdc5c6f787983d6a3c2eedf7f4ba4
Mohitkr3041 <mohitkumar9pro@gmail.com>
2026-03-30 19:11:17 +0530

    new

--- Backend/server.js
+++ Backend/server.js
@@ -3,19 +3,14 @@ require("dotenv").config();
 const app = require("./src/app");
 const connectDB = require("./src/db/db.js");
 
-const dns = require("dns")
-dns.setServers(["1.1.1.1"])
+const dns = require("dns");
+dns.setServers(["1.1.1.1"]);
 
 connectDB();
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 console.log("Starting AIstro Backend... 🚀");
-
-app.use(cors({
-  origin: "https://a-istro.vercel.app",
-  credentials: true
-}));
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
